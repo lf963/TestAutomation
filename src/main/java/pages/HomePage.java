0be8a5f1d8d91d97deec6acc067a1d9d763fb1bc
lf963 <32009158+lf963@users.noreply.github.com>
2025-06-28 15:57:50 -0400

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,6 +2,8 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 
 public class HomePage {
     private WebDriver driver;
@@ -10,6 +12,15 @@ public class HomePage {
     public HomePage(WebDriver driver){
         this.driver = driver;
     }
+
+    public WebElement getABTestingLink(){
+        return driver.findElement(By.linkText("A/B Testing"));
+    }
+
+    public WebElement getAddRemoveElementsLink(){
+        return driver.findElement(By.linkText("Add/Remove Elements"));
+    }
+
     public LoginPage clickFormAuthentication() {
         driver.findElement(formAuthenticationLink).click();
         return new LoginPage(driver);
